bc43f3fc76ed84de46b21a9682eed5418275e1fd
crystalh321 <crystalhuangbhs@gmail.com>
2018-03-31 16:16:59 -0500

    did step 7 and wrote my own Profiles.java for step 8

--- app/src/main/java/com/example/michie/timber/domain/Profile.java
+++ app/src/main/java/com/example/michie/timber/domain/Profile.java
@@ -1,4 +1,53 @@
 package com.example.michie.timber.domain;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Profile {
+
+    private String name;
+    private String url;
+    private String location;
+    private int age;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("url")
+    @Expose
+    private String imageUrl;
+
+    @SerializedName("age")
+    @Expose
+    private Integer age;
+
+    @SerializedName("location")
+    @Expose
+    private String location;
+
+    public int getAge() {
+        return age;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
